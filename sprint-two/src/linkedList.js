645260aebbdc901815c283e8fa248016cd0d2a50
chen101385 <chen1385@gmail.com>
2017-11-30 11:00:28 -0800

    Pseudocode linkedlists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,35 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //check if current is tail and if exists
+      
+    //then find tail if needed
+    //create node
+        //set value to new node value
+    //set current tail pointer to new node
+    //set list tail to new node
+
   };
 
   list.removeHead = function() {
+    //check if current head exists
+    //if so
+        //set value to null
+        //set list head to next node
+        //set current node next to null. Necessary?
+    //otherwise, nothing
+    
   };
 
   list.contains = function(target) {
+    
+    //traverse linked list
+      //create var current
+      //check each currentnode value === target
+        //if so, return true
+        //if not, move current to current next
+      //if none, return false
+
   };
 
   return list;
