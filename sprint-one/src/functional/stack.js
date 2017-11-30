5fdfe06459a3ac29462d81ecba17fad5da62fccb
chen101385 <chen1385@gmail.com>
2017-11-29 16:06:22 -0800

    add pseudocode, refactor count variable, pass all tests for stack
    No more notes

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,26 +3,36 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-  storage.howBig = 0;
+  var howBig = 0;
   // Implement the methods below
   
+  
   someInstance.push = function(value) {
-    storage.howBig++;
-    storage[storage.howBig] = value;
+    //count bigger
+    //assign count as a key for value in storage
+    howBig++;
+    storage[howBig] = value;
   };
 
   someInstance.pop = function() {
-    if (storage[storage.howBig]) {
-      var temp = storage[storage.howBig];
-      storage[storage.howBig] = undefined;
-      storage.howBig--;
+    //check for 0 length, can't pop empty stack
+    //create a pointer toward current last stack item
+    // remove last stack item by setting value to undefined
+    // lower count
+    //return pointer
+    if (storage[howBig]) {
+      var temp = storage[howBig];
+      storage[howBig] = undefined;
+      howBig--;
       return temp;
     } 
   };
 
   someInstance.size = function() {
-    return storage.howBig;
+    //return count
+    return howBig;
   };
 
   return someInstance;
+  //return instance of stack constructor function
 };
