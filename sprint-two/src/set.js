91aef9c437f66be1ecafa162a7a4169e624b792a
chen101385 <chen1385@gmail.com>
2017-12-01 15:14:05 -0800

    Add partial functionality for set
    No remove yet

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,42 @@
 var Set = function() {
+  //creating a new object via prototypal instantiation 
+    //storage container
+      //first try array
+    //return the set
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
+//the prototype stores the methods for each set
+  //Object.create establishes the prototype 
 
 setPrototype.add = function(item) {
+//input would be a value (string)
+//output would be adding the value (string) to the storage container
+  //each value is unique
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  }
+  
 };
 
 setPrototype.contains = function(item) {
+//input would be a value (string)
+//output would be a boolean
+  //T/F depending on whether container has value
+  if (this._storage.indexOf(item) !== -1) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 setPrototype.remove = function(item) {
+//input would be a value (string)
+//output would be the removal of the value from storage
+  //check whether present in set 
 };
 
 /*
