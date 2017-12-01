2e05ebd722419b2f8d8b45c80b8d1a38e8f941cd
chen101385 <chen1385@gmail.com>
2017-11-30 20:17:03 -0800

    Pseudocode graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,14 +2,26 @@
 
 // Instantiate a new graph
 var Graph = function() {
+
+//this = Object.create(Graph.prototype)
+//add node container object
+//add value property
+  // key equals counter
+  //value equals edge properties
+    //consider array or object for value data structure
+  //use counter to record value
+//add edge property (1+ edges)
+//return this
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+//add a node within node container object
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+//check node container object for the node (v)
 };
 
 // Removes a node from the graph.
