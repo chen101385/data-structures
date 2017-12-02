abc659a7249e5089e368fe2ab36a374b100f6ea3
chen101385 <chen1385@gmail.com>
2017-12-01 18:32:12 -0800

    Pass hashTable
    Problems solved: calling on buckets instead of tuples. indexOf undefined.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -12,9 +12,24 @@ HashTable.prototype.insert = function(k, v) {
   //already have index;
   //at index of storage array, assign 'tuple' to index of storage array 
     //for assigned tuple, index 0 is key; index 1 is value;
+  //no bucket yet
+  var boolean = false;
+
   if (!this._storage[index]) {
     this._storage[index] = [[k, v]];
-  } else {
+  }  
+
+  //if bucket & 1+ tuple that collide
+  if (this._storage[index].length) {
+    for (var i = 0; i < this._storage[index].length; i++) {
+      if (this._storage[index][i][0] === k) {
+        this._storage[index][i][1] = v;
+        boolean = true;
+      }
+    }
+  }
+  //if empty bucket or no overlap
+  if (!this._storage[index].length || !boolean) {
     this._storage[index].push([k, v]);
   }
 };
@@ -26,9 +41,9 @@ HashTable.prototype.retrieve = function(k) {
   //return the tuple @ index
 //ADD TEST check whether index occupied
   if (this._storage[index].length) {
-    for (var i = 0; i < this._storage.length; i++) {
+    for (var i = 0; i < this._storage[index].length; i++) {
       if (this._storage[index][i][0] === k) {
-        return this._storage[index][i];
+        return this._storage[index][i][1];
       }
     }
   }
@@ -41,9 +56,12 @@ HashTable.prototype.remove = function(k) {
   //if first tuple's value = key, remove tuple
     //if first tuple's value doe not equal key, then search remaining tuples;
   if (this._storage[index].length) {
-    for (var i = 0; i < this._storage.length; i++) {
+    for (var i = 0; i < this._storage[index].length; i++) {
       if (this._storage[index][i][0] === k) {
-        this._storage[index].splice(indexOf(this._storage[index][i]), 1);
+        this._storage[index].splice(this._storage[index].indexOf(this._storage[index][i]), 1);
+//I suspect that bucket [steven, tyler], instead of [[steven, tyler]]
+//if bucket is nested, [index][i]
+  //if bucket has 1 value [index]
       }
     }
   }
